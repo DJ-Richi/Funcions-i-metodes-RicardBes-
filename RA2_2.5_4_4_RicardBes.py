aae2c0f911b008c31697fb06d93f6d0171cfc77f
DJ-Richi <rbesguimera@gmail.com>
2025-11-07 13:07:15 +0100

    Add files via upload

---
+++ RA2_2.5_4_4_RicardBes.py
@@ -0,0 +1,12 @@
+# Administració de Sistemes Informàtics en Xarxa
+# 
+# Autor: Ricard Bes Guimerà
+# Data: 07/11/2024
+# Versió: 1
+#
+# Descripció: 4. Comprova si diferents nombres són parells
+# Defineix es_parell(numero).
+# Per cada número de la llista [1, 2, 3, 4, 5, 6], crida la funció i imprimeix:
+# "El número X és parell: True/False"
+# Especificacions: Programa que que mostri per pantalla la llista de numeros i que digui si es parrell algun numero de llista.
+
